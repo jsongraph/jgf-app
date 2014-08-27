4ad4f6d1fbfc52d5fcb23a2f93486a1a2b8cca6f
Anthony Bargnesi <abargnesi@selventa.com>
2014-08-27 07:32:47 -0400

    include maven in tree; one less dev requirement

--- env.sh
+++ env.sh
@@ -7,6 +7,7 @@ export DEV_SOURCE_DIR="${DEV_SOURCE_DIR:=$DEV_DIR/src}"
 export DEV_SCRIPTS_DIR="${DEV_SCRIPTS_DIR:=$DEV_DIR/scripts}"
 export DEV_TOOLS_DIR="${DEV_TOOLS_DIR:=$DEV_DIR/tools}"
 export DEV_CY3_DIR="${DEV_CY3_DIR:=$DEV_TOOLS_DIR/cytoscape}"
+export DEV_MAVEN_DIR="${DEV_MAVEN_DIR:=$DEV_TOOLS_DIR/maven}"
 export DEV_CY3_LIBS_DIR="${DEV_CY3_LIBS_DIR:=$DEV_CY3_DIR/framework/system}"
 export DEV_CY3_WORK_DIR="${DEV_CY3_WORK_DIR:=$DEV_CY3_DIR/work}"
 export DEV_CY3_BUNDLE_DIR="${DEV_CY3_BUNDLE_DIR:=$DEV_CY3_WORK_DIR/bundles}"
@@ -15,3 +16,6 @@ export DEV_CY3_DATA_DIR="${DEV_CY3_DATA_DIR:=$DEV_CY3_WORK_DIR/data}"
 # -- Files --
 export DEV_TOOLS_JAVA_REPL_FILE="${DEV_JAVA_REPL_FILE:=$DEV_TOOLS_DIR/javarepl.jar}"
 export DEV_CY3_LOG_FILE="${DEV_CY3_LOG_FILE:=$DEV_CY3_WORK_DIR/cytoscape.log}"
+
+# -- Vars --
+export PATH="$DEV_MAVEN_DIR/bin:$PATH"
