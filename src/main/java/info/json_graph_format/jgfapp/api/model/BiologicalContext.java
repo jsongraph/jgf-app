95c6707daae3e439606843f3ae68a609a0e50a0f
Anthony Bargnesi <abargnesi@selventa.com>
2015-07-10 16:26:36 -0400

    flatten annotations to biological_context property
    define JSON property names according to JGF

--- src/main/java/info/json_graph_format/jgfapp/api/model/BiologicalContext.java
+++ src/main/java/info/json_graph_format/jgfapp/api/model/BiologicalContext.java
@@ -1,5 +1,9 @@
 package info.json_graph_format.jgfapp.api.model;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,7 +13,17 @@ import java.util.Map;
  */
 public class BiologicalContext {
 
+    @JsonProperty("ncbi_tax_id")
     public Integer ncbiTaxId;
+
+    @JsonProperty("species_common_name")
     public String speciesCommonName;
+
+    @JsonIgnore
     public Map<String, Object> variedAnnotations = new HashMap<String, Object>();
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return variedAnnotations;
+    }
 }
