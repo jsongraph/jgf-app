6df77f70e135b2590d52f46b80ec2e94490fc701
Anthony Bargnesi <abargnesi@selventa.com>
2015-07-13 10:48:58 -0400

    version to 2.0.0; updated changelog / app metadata

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,28 @@
 Changelog
 =========
 
+Version 2.0.0
+-------------
+
+### Important Note
+
+This version of JGF App requires Java 8 to be installed on your system. [Download Java 8](https://java.com/en/download/) and install.
+
+### Changes
+
+- Fixes
+    - Show *Summary Text* when viewing evidence ([#9](https://github.com/jsongraph/jgf-app/issues/9)).
+
+- Enhancements
+    - List evidence panel providing create, edit, and delete actions.
+    - Create new evidence.
+    - Edit existing evidence.
+    - Delete existing evidence.
+
+
 Version 1.0.2
 -------------
 
 - Enhancements
-  - Integrated help documentation ([#6](https://github.com/jsongraph/jgf-app/issues/6))
-  - Added About dialog with description and links ([#7](https://github.com/jsongraph/jgf-app/issues/7))
+    - Integrated help documentation ([#6](https://github.com/jsongraph/jgf-app/issues/6))
+    - Added About dialog with description and links ([#7](https://github.com/jsongraph/jgf-app/issues/7))
--- src/main/java/info/json_graph_format/jgfapp/api/JGFApp.java
+++ src/main/java/info/json_graph_format/jgfapp/api/JGFApp.java
@@ -14,14 +14,14 @@ public class JGFApp {
 
     public static final String NAME = "JGF App";
     public static final String DESC =
-            "This application provides import of Cytoscape Networks in " +
-                    "JSON Graph Format. Additionally it can visualize BEL networks " +
-                    "and show evidence detail on edges.";
-    public static final String VERSION = "1.0.2";
+            "This application provides import of Cytoscape Networks in JSON Graph Format. " +
+                    "Additionally it can view, create, edit and delete edge evidence when visualizing BEL networks. " +
+                    "Important note: This version requires Java 8 to be installed on your system.";
+    public static final String VERSION = "2.0.0";
     public static final Icon ICON = new ImageIcon(JGFApp.class.getResource("/icon.png"));
     public static final Collection<Pair<String, String>> LINKS = asList(
-            new Pair<String, String>("Source code", "https://github.com/jsongraph/jgf-app"),
-            new Pair<String, String>("Submit issues", "https://github.com/jsongraph/jgf-app/issues")
+            new Pair<>("Source code", "https://github.com/jsongraph/jgf-app"),
+            new Pair<>("Submit issues", "https://github.com/jsongraph/jgf-app/issues")
     );
 
     private JGFApp() {
